aa0ce2de1c08e2ebe6af76566ef67fc003817462
rondDev <contact@rond.cc>
2025-05-14 23:05:06 +0200

    fix: add user to locals

--- src/app.d.ts
+++ src/app.d.ts
@@ -3,7 +3,13 @@
 declare global {
 	namespace App {
 		// interface Error {}
-		// interface Locals {}
+		interface Locals {
+			user: {
+				id: string;
+				username: string;
+				uploadKey: string;
+			};
+		}
 		// interface PageData {}
 		// interface PageState {}
 		// interface Platform {}
