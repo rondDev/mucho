79b997efcc173922fa709d8dacea10ecdb70224c
rondDev <contact@rond.cc>
2025-05-14 23:08:47 +0200

    fix: add db types

--- src/lib/db/dbTypes.ts
+++ src/lib/db/dbTypes.ts
@@ -1,27 +1,27 @@
 import type {
-  Generated,
-  Insertable,
-  JSONColumnType,
-  Selectable,
-  Updateable,
+	Generated,
+	Insertable,
+	JSONColumnType,
+	Selectable,
+	Updateable,
 } from 'kysely';
 
 export interface Database {
-  users: UsersTable;
-  sessions: SessionsTable;
+	users: UsersTable;
+	sessions: SessionsTable;
+	file_upload_keys: UploadKeysTable;
 }
 
 export interface UsersTable {
-  id: Generated<string>;
-  createdAt: Generated<Date>;
-  updatedAt: Date;
-  email: string;
-  password: string;
-  description: string | null;
-  prefs: JSONColumnType<Record<string | number | symbol, never>>;
-  profilePicture: Generated<string>;
-  sessionJwt: string;
-  sessionId: string;
+	id: Generated<string>;
+	createdAt: Generated<Date>;
+	updatedAt: Generated<Date>;
+	email: string | null;
+	username: string;
+	password: string;
+	description: string | null;
+	prefs: JSONColumnType<Record<string | number | symbol, never>> | null;
+	profilePicture: Generated<string>;
 }
 
 export type User = Selectable<UsersTable>;
@@ -29,13 +29,26 @@ export type NewUser = Insertable<UsersTable>;
 export type UserUpdate = Updateable<UsersTable>;
 
 export interface SessionsTable {
-  createdAt: Generated<Date>;
-  updatedAt: Date;
-  userId: string;
-  expire: Date;
-  ip: string;
+	id: string;
+	sessionToken: string;
+	createdAt: Generated<Date>;
+	updatedAt: Generated<Date>;
+	userId: string;
+	expire: Date;
 }
 
 export type Session = Selectable<SessionsTable>;
 export type NewSession = Insertable<SessionsTable>;
 export type SessionUpdate = Updateable<SessionsTable>;
+
+export interface UploadKeysTable {
+	id: string;
+	uploadKey: string;
+	userId: string;
+	createdAt: Generated<Date>;
+	updatedAt: Generated<Date>;
+}
+
+export type UploadKeys = Selectable<UploadKeysTable>;
+export type NewUploadKeys = Insertable<UploadKeysTable>;
+export type UploadKeysUpdate = Updateable<UploadKeysTable>;
