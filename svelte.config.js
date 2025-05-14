a77d9cdc7154312aa2bc7f3d499c04632b190dff
rondDev <contact@rond.cc>
2025-05-14 23:05:21 +0200

    fix: add aliases to svelte config

--- svelte.config.js
+++ svelte.config.js
@@ -3,7 +3,13 @@ import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 const config = {
 	preprocess: vitePreprocess(),
-	kit: { adapter: adapter() }
+	kit: { adapter: adapter() },
+	alias: {
+		$lib: './src/lib',
+		'$lib/*': './src/lib/*',
+		$components: './src/components',
+		'$components/*': './src/components/*',
+	},
 };
 
 export default config;
