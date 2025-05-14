da23bc5859476dc4f7224cffe78b52db841ef5eb
rondDev <contact@rond.cc>
2025-05-14 23:07:11 +0200

    fix: add pino logger types

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,1 +1,13 @@
 // place files you want to import through the `$lib` alias in this folder.
+import type { Logger, LoggerOptions } from 'pino';
+
+export enum ServerEnvironment {
+	DEV = 0,
+	PREV = 1,
+	STG = 2,
+	PROD = 3,
+}
+
+export type PinoLogger = Logger & {
+	setLogLevel?: (NODE_ENV: ServerEnvironment) => LoggerOptions['level'];
+};
