923a7cd7ce7f9272ff2bc6d3965ea110d77710e0
rondDev <contact@rond.cc>
2025-05-14 23:09:23 +0200

    fix: add server hooks

---
+++ src/hooks.server.ts
@@ -0,0 +1,34 @@
+import type { Handle, RequestEvent } from '@sveltejs/kit';
+import { db } from '$lib/db/database';
+
+async function getUser(session: string, event: RequestEvent) {
+	const user = await db
+		.selectFrom('users')
+		.innerJoin('sessions', 'sessions.userId', 'users.id')
+		.innerJoin('file_upload_keys', 'file_upload_keys.userId', 'users.id')
+		.where('sessions.sessionToken', '=', session)
+		.select(['users.username', 'file_upload_keys.uploadKey', 'users.id'])
+		.executeTakeFirst();
+	if (user) {
+		event.locals.user = user;
+	}
+}
+
+export const handle: Handle = async ({ event, resolve }) => {
+	const { cookies, url } = event;
+	const session = cookies.get('session');
+
+	if (session) {
+		await getUser(session, event);
+	}
+
+	if (!event.locals.user && !url.pathname.includes('api'))
+		cookies.delete('session', { path: '/' });
+
+	const response = await resolve(event);
+
+	response.headers.append('cache-control', 'no-store');
+	response.headers.append('vary', 'cookie');
+
+	return response;
+};
