30c7da8dd99117649e5242d91df40e468d6d441b
rondDev <contact@rond.cc>
2025-05-14 08:15:18 +0200

    feat: add registration

---
+++ src/routes/register/+page.server.ts
@@ -0,0 +1,69 @@
+import { fail, redirect } from '@sveltejs/kit';
+import { v4 as uuidv4 } from 'uuid';
+import type { Action, Actions, PageServerLoad } from './$types';
+import * as argon2 from 'argon2';
+
+import { db } from '$lib/db/database';
+import { createId } from '@paralleldrive/cuid2';
+import pino from 'pino';
+import { logger } from '$lib/stores/logger';
+
+// TODO: Add session check
+// export const load: PageServerLoad = async ({ locals }) => {
+//   if (locals.user) {
+//     throw redirect(302, '/');
+//   }
+// };
+
+const register: Action = async ({ request }) => {
+	try {
+		const data = await request.formData();
+		const username = data.get('username');
+		const password = data.get('password');
+
+		// TODO: Introduce enable/disable registration
+
+		if (
+			typeof username !== 'string' ||
+			typeof password !== 'string' ||
+			!username ||
+			!password
+		) {
+			return fail(400, { invalid: true });
+		}
+
+		const user = await db
+			.selectFrom('users')
+			.where('users.username', '=', username)
+			.executeTakeFirst();
+		if (user) {
+			return fail(400, { user: true });
+		}
+
+		// TODO: Add password requirements
+		const userId = createId();
+		const registered = await db
+			.insertInto('users')
+			.values({
+				id: userId,
+				username: username,
+				password: await argon2.hash(password),
+			})
+			.executeTakeFirstOrThrow();
+
+		await db
+			.insertInto('file_upload_keys')
+			.values({
+				id: createId(),
+				uploadKey: uuidv4(),
+				userId: userId,
+			})
+			.execute();
+	} catch (e) {
+		logger.error(e, '[register | +page.server.ts | catch]');
+	}
+
+	throw redirect(303, '/login?justRegistered=true');
+};
+
+export const actions: Actions = { register };
---
+++ src/routes/register/+page.svelte
@@ -0,0 +1,66 @@
+<script lang="ts">
+  import type { ActionData } from "./$types";
+  import { PUBLIC_DOMAIN } from "$env/static/public";
+
+  export let form: ActionData;
+</script>
+
+<svelte:head>
+  <title>Register - {PUBLIC_DOMAIN}</title>
+</svelte:head>
+
+<div class="flex flex-col gap-10 text-white">
+  <h1 class="text-3xl font-semibold">Register</h1>
+  <form
+    class="flex flex-col w-[22rem] gap-16 text-white"
+    action="?/register"
+    method="POST"
+  >
+    <div class="flex flex-col gap-4">
+      <div class="m-1">
+        <label class="label pb-2" for="username">Username</label>
+        <input
+          class="input h-12 p-5"
+          id="username"
+          name="username"
+          type="text"
+          required
+        />
+      </div>
+      <div class="m-1">
+        <label class="label pb-2" for="password">Password</label>
+        <input
+          class="input h-12 p-5"
+          id="password"
+          name="password"
+          type="password"
+          required
+        />
+      </div>
+    </div>
+
+    {#if form?.user}
+      <p class="error">Username is taken.</p>
+    {/if}
+
+    {#if form?.registrationdisabled}
+      <p class="error">Registration is disabled.</p>
+    {/if}
+
+    {#if form?.invalid}
+      <p class="error">INVALID!</p>
+    {/if}
+
+    <button class="btn variant-filled-primary h-12" type="submit"
+      >Register</button
+    >
+    <p class="self-end mt-[-2rem]">
+      Already have an account? <a
+        class="hover:underline btn-sm variant-form-material"
+        href="/login"
+      >
+        Log in</a
+      >
+    </p>
+  </form>
+</div>
