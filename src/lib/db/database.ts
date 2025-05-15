4360b183f6069cc7bf50046c0ed2c20bc499be14
rondDev <contact@rond.cc>
2025-05-15 02:28:29 +0200

    fix: formatting

--- src/lib/db/database.ts
+++ src/lib/db/database.ts
@@ -2,23 +2,23 @@ import type { Database } from './dbTypes'; // this is the Database interface we 
 import { Pool } from 'pg';
 import { Kysely, PostgresDialect } from 'kysely';
 import {
-  DB_DATABASE,
-  DB_HOST,
-  DB_USER,
-  DB_PASSWORD,
-  DB_PORT,
-  DB_MAXCONCURRENT,
+	DB_DATABASE,
+	DB_HOST,
+	DB_USER,
+	DB_PASSWORD,
+	DB_PORT,
+	DB_MAXCONCURRENT,
 } from '$env/static/private';
 
 const dialect = new PostgresDialect({
-  pool: new Pool({
-    database: DB_DATABASE,
-    host: DB_HOST,
-    user: DB_USER,
-    password: DB_PASSWORD,
-    port: Number.parseInt(DB_PORT || '5432'),
-    max: Number.parseInt(DB_MAXCONCURRENT || '10'),
-  }),
+	pool: new Pool({
+		database: DB_DATABASE,
+		host: DB_HOST,
+		user: DB_USER,
+		password: DB_PASSWORD,
+		port: Number.parseInt(DB_PORT || '5432'),
+		max: Number.parseInt(DB_MAXCONCURRENT || '10'),
+	}),
 });
 
 // Database interface is passed to Kysely's constructor, and from now on, Kysely
@@ -26,5 +26,5 @@ const dialect = new PostgresDialect({
 // Dialect is passed to Kysely's constructor, and from now on, Kysely knows how
 // to communicate with your database.
 export const db = new Kysely<Database>({
-  dialect,
+	dialect,
 });
