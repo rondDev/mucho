27f71ce5d6f36f8d826dedb6139f19c38aea9f94
rondDev <contact@rond.cc>
2025-05-15 22:59:56 +0200

    fix: add user check for /

---
+++ src/routes/+page.server.ts
@@ -0,0 +1,13 @@
+import { redirect } from '@sveltejs/kit';
+
+export function load({ locals }: { locals: App.Locals }) {
+	if (!locals.user) {
+		// throw redirect(301, '/login');
+		return {
+			user: undefined,
+		};
+	}
+	return {
+		user: locals.user,
+	};
+}
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,1 +1,10 @@
-Hi
+<script lang="ts">
+import Navigation from '$lib/components/Navigation.svelte';
+
+const { data } = $props();
+</script>
+{#if data.user}
+  <Navigation />
+{:else}
+  Hi
+{/if}
