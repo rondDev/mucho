c24e8fe72dc9cd25fa4c04d5e8698e91248d106f
rondDev <contact@rond.cc>
2025-05-16 00:50:41 +0200

    fix: try to add cors headers

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -15,6 +15,25 @@ async function getUser(session: string, event: RequestEvent) {
 }
 
 export const handle: Handle = async ({ event, resolve }) => {
+	if (event.url.pathname.startsWith('/api')) {
+		// Required for CORS to work
+		if (event.request.method === 'OPTIONS') {
+			return new Response(null, {
+				headers: {
+					'Access-Control-Allow-Methods':
+						'GET, POST, PUT, DELETE, PATCH, OPTIONS',
+					'Access-Control-Allow-Origin': '*',
+					'Access-Control-Allow-Headers': '*',
+				},
+			});
+		}
+
+		const response = await resolve(event);
+		if (event.url.pathname.startsWith('/api')) {
+			response.headers.append('Access-Control-Allow-Origin', '*');
+			return response;
+		}
+	}
 	const { cookies, url } = event;
 	const session = cookies.get('session');
 
