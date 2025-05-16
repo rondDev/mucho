b33c70300c2eba7c55542428c9a04019ea16733c
rondDev <contact@rond.cc>
2025-05-16 17:11:14 +0200

    fix: add attributify

--- uno.config.ts
+++ uno.config.ts
@@ -1,7 +1,7 @@
-import { defineConfig, presetWind4 } from 'unocss';
+import { defineConfig, presetAttributify, presetWind4 } from 'unocss';
 
 export default defineConfig({
-	presets: [presetWind4({ reset: true })],
+	presets: [presetWind4({ reset: true }), presetAttributify()],
 	rules: [],
 	shortcuts: [
 		{
