b8fd6612ebb8e853e53bf5a32385f9cc5ef0d20b
rondDev <contact@rond.cc>
2025-05-16 23:52:58 +0200

    fix: add option to use umami

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,16 @@
 <script lang="ts">
-import Navigation from '$lib/components/Navigation.svelte';
+import {
+	PUBLIC_UMAMI_DOMAIN,
+	PUBLIC_UMAMI_WEBSITE_ID,
+} from '$env/static/public';
 import 'virtual:uno.css';
 </script>
+
+<svelte:head>
+  {#if PUBLIC_UMAMI_DOMAIN}
+    <script defer src={PUBLIC_UMAMI_DOMAIN} data-website-id={PUBLIC_UMAMI_WEBSITE_ID}></script>
+  {/if}
+</svelte:head>
 
 <div
   class="bg-[#09090b] text-[#fafafa] absolute min-h-[100vh] min-w-[100vw] bg-[#09090b] text-[#fafafa]"
