76ef41af977626156f46f8acab06b322ea030e45
rondDev <contact@rond.cc>
2025-05-28 09:37:20 +0200

    fix: add a homepage

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,10 +1,21 @@
 <script lang="ts">
-import Navigation from '$lib/components/Navigation.svelte';
+  import Navigation from "$lib/components/Navigation.svelte";
 
-const { data } = $props();
+  const { data } = $props();
 </script>
+
 {#if data.user}
   <Navigation />
 {:else}
-  Hi
+  <div
+    class="help w-full h-[100vh] flex flex-col gap-4 justify-center items-center bg-gradient-to-br from-cyan-300 to-pink-500"
+  >
+    <div class="flex flex-col gap-2 relative">
+      <p class="text-[6rem] ml-0">mucho</p>
+      <p class="absolute bottom-1 left-1">Screenshot uploader and manager</p>
+    </div>
+    <div>
+      <a href="/login" class="bg-orange p-3 rounded-md">Login</a>
+    </div>
+  </div>
 {/if}
