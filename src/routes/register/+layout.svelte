5dc164a3f66d9a2e40e96a152d98345eae2da858
rondDev <contact@rond.cc>
2025-05-28 09:37:05 +0200

    feat: add layout to login and register

---
+++ src/routes/register/+layout.svelte
@@ -0,0 +1,9 @@
+<div class="h-screen w-full flex bg-slate-950">
+  <div
+    class="hidden flex-col grow h-full justify-center items-center bg-slate-900 lg:flex w-1/2"
+    style="background-image: url(https://images.unsplash.com/photo-1608142521278-d2da5905428e?ixlib=rb-4.0.3&q=85&fm=jpg&crop=entropy&cs=srgb); background-size: cover; background-position: center;"
+  ></div>
+  <div class="flex grow h-full justify-center items-center">
+    <slot />
+  </div>
+</div>
