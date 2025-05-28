2fb330b59bf308170a19757a8c756563acc07127
rondDev <contact@rond.cc>
2025-05-28 09:36:57 +0200

    fix: formatting

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,19 +1,23 @@
 <script lang="ts">
-import {
-	PUBLIC_UMAMI_DOMAIN,
-	PUBLIC_UMAMI_WEBSITE_ID,
-} from '$env/static/public';
-import 'virtual:uno.css';
+	import {
+		PUBLIC_UMAMI_DOMAIN,
+		PUBLIC_UMAMI_WEBSITE_ID,
+	} from "$env/static/public";
+	import "virtual:uno.css";
 </script>
 
 <svelte:head>
-  {#if PUBLIC_UMAMI_DOMAIN}
-    <script defer src={PUBLIC_UMAMI_DOMAIN} data-website-id={PUBLIC_UMAMI_WEBSITE_ID}></script>
-  {/if}
+	{#if PUBLIC_UMAMI_DOMAIN}
+		<script
+			defer
+			src={PUBLIC_UMAMI_DOMAIN}
+			data-website-id={PUBLIC_UMAMI_WEBSITE_ID}
+		></script>
+	{/if}
 </svelte:head>
 
 <div
-  class="bg-[#09090b] text-[#fafafa] absolute min-h-[100vh] min-w-[100vw] bg-[#09090b] text-[#fafafa]"
+	class="bg-[#09090b] text-[#fafafa] absolute min-h-[100vh] min-w-[100vw] text-[#fafafa]"
 >
-  <slot />
+	<slot />
 </div>
