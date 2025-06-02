0d38cb30361b56b89419cc7d890a6e7f2e5c90f7
rondDev <contact@rond.cc>
2025-06-02 21:38:36 +0200

    feat: add stats to homepage when user is logged in

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,13 +1,119 @@
-import { redirect } from '@sveltejs/kit';
+import { db } from '$lib/db/database';
+import { s3Client } from '$lib/s3';
+import { logger } from '$lib/stores/logger';
+import { bytesToSize } from '$lib/utils';
 
-export function load({ locals }: { locals: App.Locals }) {
+export async function load({ locals }: { locals: App.Locals }) {
 	if (!locals.user) {
-		// throw redirect(301, '/login');
 		return {
 			user: undefined,
 		};
 	}
+	const data = await getData(locals)
 	return {
 		user: locals.user,
+		data
 	};
 }
+
+
+async function getData(locals: App.Locals) {
+	try {
+		const log = logger.child({ location: "+page.server.ts", context: { user: locals.user } })
+		const userFileCount = await db
+			.selectFrom('files')
+			.select([(c) => c.fn.count('id').as('count')])
+			.where('userId', '=', locals.user.id)
+			.executeTakeFirstOrThrow();
+		log.info({ msg: 'userFileCount', content: userFileCount });
+
+		if (!userFileCount.count) {
+			return {
+				error: 'Could not get amount of files for user',
+			};
+		}
+
+		const totalFileCount = await db
+			.selectFrom('files')
+			.select([(c) => c.fn.count('id').as('count')])
+			.executeTakeFirstOrThrow();
+		log.info('totalFileCount', totalFileCount);
+
+		const userCount = await db
+			.selectFrom('users')
+			.select([(c) => c.fn.count('id').as('count')])
+			.executeTakeFirstOrThrow();
+
+		if (!totalFileCount) {
+			return {
+				error: 'Could not get amount of total files',
+			};
+		}
+
+		const allObjects = await s3Client.list({});
+
+		let totalSize = 0;
+		let totalUserSize = 0;
+		if (allObjects.contents) {
+			for (const o of allObjects.contents) {
+				if (o.size) {
+					totalSize += o.size;
+					if (o.key.startsWith(locals.user.username)) {
+						totalUserSize += o.size;
+					}
+				}
+			}
+		}
+
+		log.info({
+			msg: "Return object", content: {
+				returnObject: {
+					userTotal: userFileCount.count,
+					total: totalFileCount.count,
+					userSize: bytesToSize(totalUserSize),
+					totalSize: bytesToSize(totalSize),
+					userCount: userCount.count,
+				}
+			}
+		});
+
+		return {
+			userTotal: userFileCount.count,
+			total: totalFileCount.count,
+			userSize: bytesToSize(totalUserSize),
+			totalSize: bytesToSize(totalSize),
+			userCount: userCount.count,
+		};
+
+		// const fileData = await db
+		//   .selectFrom('files')
+		//   .where('fileName', '=', fileUrl)
+		//   .selectAll()
+		//   .executeTakeFirst();
+		//
+		// if (!fileData) {
+		//   return {
+		//     error: 'File not found',
+		//   };
+		// }
+		//
+		// const file = await s3Client.send(
+		//   new GetObjectCommand({
+		//     Bucket: fileData.bucket,
+		//     Key: fileData.key,
+		//   }),
+		// );
+		// const s = file.Body?.transformToWebStream();
+		// if (!s) {
+		//   return {
+		//     error: 'No file found',
+		//   };
+		// }
+		// return sendStream(event, s);
+	} catch (e) {
+		logger.child({ location: "+page.server.ts", context: { user: locals.user } }).error(e)
+		return {
+			error: e,
+		};
+	}
+};
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,10 +1,68 @@
 <script lang="ts">
-  import Navigation from "$lib/components/Navigation.svelte";
+  import Navigation from '$lib/components/Navigation.svelte';
+  import Icon from '@iconify/svelte';
 
   const { data } = $props();
+  const stats = $derived(data.data);
 </script>
 
 {#if data.user}
+  <div class="h-[100vh] flex items-center justify-center">
+    <div class="grid grid-cols-2 gap-8">
+      <div class="flex flex-col w-64 h-36 rounded-lg border border-[#888888]">
+        <div class="flex items-center mt-4 ml-4">
+          <Icon
+            icon="gravity-ui:clock"
+            height="24px"
+            width="24px"
+            class={`color-white menuBarButton`}
+            mode="svg"
+          />
+          <p class="text-lg ml-2">Your files</p>
+        </div>
+        <p class="text-4xl mr-6 ml-auto mb-6 mt-auto">{stats?.userTotal}</p>
+      </div>
+      <div class="flex flex-col w-64 h-36 rounded-lg border border-[#888888]">
+        <div class="flex items-center mt-4 ml-4">
+          <Icon
+            icon="gravity-ui:clock"
+            height="24px"
+            width="24px"
+            class={`color-white menuBarButton`}
+            mode="svg"
+          />
+          <p class="text-lg ml-2">Total size of your files</p>
+        </div>
+        <p class="text-4xl mr-6 ml-auto mb-6 mt-auto">{stats?.userSize}</p>
+      </div>
+      <div class="flex flex-col w-64 h-36 rounded-lg border border-[#888888]">
+        <div class="flex items-center mt-4 ml-4">
+          <Icon
+            icon="gravity-ui:clock"
+            height="24px"
+            width="24px"
+            class={`color-white menuBarButton`}
+            mode="svg"
+          />
+          <p class="text-lg ml-2">Total files</p>
+        </div>
+        <p class="text-4xl mr-6 ml-auto mb-6 mt-auto">{stats?.total}</p>
+      </div>
+      <div class="flex flex-col w-64 h-36 rounded-lg border border-[#888888]">
+        <div class="flex items-center mt-4 ml-4">
+          <Icon
+            icon="gravity-ui:clock"
+            height="24px"
+            width="24px"
+            class={`color-white menuBarButton`}
+            mode="svg"
+          />
+          <p class="text-lg ml-2">Total size of all files</p>
+        </div>
+        <p class="text-4xl mr-6 ml-auto mb-6 mt-auto">{stats?.totalSize}</p>
+      </div>
+    </div>
+  </div>
   <Navigation />
 {:else}
   <div
